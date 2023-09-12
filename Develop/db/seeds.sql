ebb00baed96a200a1fec3206e5b68c8c03036262
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-09-11 20:09:37 -0600

    adding to db

---
+++ Develop/db/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO `drinks' (`name`, `description`, `price`, `image_url`, `category_id`) VALUES ('Coca Cola', 'Coca Cola', 1.99, 'https://www.coca-cola.com.sg/content/dam/GO/CokeZone/Common/ShareACokeSG/ShareACokeSG_1.png', 1);
+
