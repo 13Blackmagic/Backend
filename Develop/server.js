b490fc4486f1332fc19a14fc30038eacba260ccf
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-10-06 09:32:33 -0600

    working in server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,12 @@
+const path = require('path');
 const express = require('express');
+const exphbs = require('express-handlebars');
 const session = require('express-session');
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -17,6 +20,17 @@ const sess = {
     db: sequelize
   })
 };
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(routes);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use(session(sess));
 
